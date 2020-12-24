5e612f06414538542f18dd77911e70a5f38d2ae8
jorgev259 <jorgev259@outlook.es>
2020-12-23 22:18:51 -0600

    remove log

--- rules/events.js
+++ rules/events.js
@@ -2,7 +2,6 @@ module.exports = {
   async messageReactionAdd (client, sequelize, module, reaction, user) {
     if (reaction.partial) await reaction.fetch()
 
-    console.log(reaction)
     const guild = client.guilds.cache.first()
     const item = await sequelize.models.module.findOne({ where: { module, guild: guild.id } })
     if (item.value && reaction.message.channel.name === 'rules') {
