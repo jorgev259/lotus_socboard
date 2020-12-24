f301c47c6dce9df4a0c60201b2c0519307c607c9
jorgev259 <jorgev259@outlook.es>
2020-12-23 22:14:26 -0600

    update for partials

--- rules/events.js
+++ rules/events.js
@@ -1,5 +1,16 @@
 module.exports = {
-  guildMemberAdd (client, sequelize, model, member) {
-    member.roles.add(member.guild.roles.cache.find(r => r.name === 'Shipmates'))
+  async messageReactionAdd (client, sequelize, module, reaction, user) {
+    if (reaction.partial) await reaction.fetch()
+
+    console.log(reaction)
+    const guild = client.guilds.cache.first()
+    const item = await sequelize.models.module.findOne({ where: { module, guild: guild.id } })
+    if (item.value && reaction.message.channel.name === 'rules') {
+      if (reaction.emoji.name === 'futabayes') {
+        const member = await guild.members.fetch(user.id)
+        await member.roles.add(guild.roles.cache.find(r => r.name === 'Shipmates'))
+      }
+      reaction.remove()
+    }
   }
 }
