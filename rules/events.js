674d6594b66b3c0a537a7c5debc88fc3d7b2e8a2
jorgev259 <jorgev259@outlook.es>
2020-12-23 21:50:56 -0600

    remove console log

--- rules/events.js
+++ rules/events.js
@@ -1,6 +1,5 @@
 module.exports = {
   guildMemberAdd (client, sequelize, model, member) {
-    console.log(member)
     member.roles.add(member.guild.roles.cache.find(r => r.name === 'Shipmates'))
   }
 }
